9bd162d228ba7811c412740a0a5aee3e56121b23
Weder Videira Borges <wederborgesemp@gmail.com>
2025-11-30 18:09:15 -0400

    Estruturando novas rotas, pages com bootstrap

--- agenda/routes.py
+++ agenda/routes.py
@@ -5,13 +5,25 @@ from flask import url_for, render_template
 
 @app.route("/")
 def home():
-    return render_template('base.html')
+    return render_template('homepage.html')
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
+@app.route("/registre")
+def registre_se():
+    return render_template("registre.html")
+
 @app.route('/marcar-horario')
 def marcar_horario():
     return render_template("marcar_horario.html")
+
+@app.route('/perfil-usuario')
+def perfil_usuario():
+    return render_template("perfil_usuario.html")
+
+@app.route('/perfil_profissional')
+def perfil_profissional():
+    return render_template("perfil_profissional.html")
 
